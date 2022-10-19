fbcb9d4936ddbe7bf158bf553e07c63a67232c6c
TagZK <julien.davidou@gmail.com>
2022-10-19 15:36:36 +0200

    Setup front

---
+++ front/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+       '^/gallery-socket-io/.*': {
+          target: 'http://localhost:3030',
+          ws: true,
+          secure: false,
+          changeOrigin: true,
+       },
+    }
+ },
+})
